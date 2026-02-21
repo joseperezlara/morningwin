432593936055b1d7deb771df6a4d611f0031834f
joseperezlara <jose.perez@consultax.mx>
2026-02-21 10:24:48 -0600

    S1.9: Fix CORS headers for production
    - Added Access-Control-Allow-Credentials header
    - Set specific origin for CORS (https://www.morningwin.app)
    - Cloud Functions now fully CORS-enabled for production

--- functions/src/functions/completeDay.ts
+++ functions/src/functions/completeDay.ts
@@ -43,7 +43,8 @@ export interface CompleteDayResponse {
 
 export const completeDay = functions.https.onRequest(async (req, res) => {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.morningwin.app');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
